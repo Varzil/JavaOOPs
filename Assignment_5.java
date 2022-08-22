e2759ec14bb0b7b82be8792cc0bf8f1e5474fbe4
Varzil <varzilthakkarvvv@gmail.com>
2022-08-22 22:59:30 +0530

    Adding Assignment_5 💻

--- Assignment_5.java
+++ Assignment_5.java
@@ -1,13 +1,14 @@
+// Write a program in Java to find maximum of three numbers using conditional operator.
 import javax.swing.*;
 
-public class Assignment_4 {
+public class Assignment_5{
     public static void main(String[] args) {
         int a, b, c, max;
         a = Integer.parseInt(JOptionPane.showInputDialog("Enter First number:"));
         b = Integer.parseInt(JOptionPane.showInputDialog("Enter Second number:"));
         c = Integer.parseInt(JOptionPane.showInputDialog("Enter Third number:"));
 
-        max=Math.max(Math.max(a,b),c);
+        max=(a>b)?Math.max(a,c):Math.max(b,c);
 //        if(a > b) {
 //            max = Math.max(a, c);
 //        }
