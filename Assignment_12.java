450b01591fe286f2082785ac5abc303e605bb9ed
Varzil <varzilthakkarvvv@gmail.com>
2022-08-25 23:41:54 +0530

    Adding Assignments_12 and 14 💻

--- Assignment_12.java
+++ Assignment_12.java
@@ -1,2 +1,34 @@
+//Write a Java Program to find area of Geometric figures using method Overloading.
+
+import java.util.Scanner;
 public class Assignment_12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double area;
+        System.out.println("Triangle: ");
+        System.out.println("Enter length of three sides of the triangle");
+        area = area(sc.nextDouble(), sc.nextDouble(), sc.nextDouble());
+        System.out.println("Area of triangle: " + area);
+
+        System.out.println("Circle:");
+        System.out.print("Enter radius of circle: ");
+        area = area(sc.nextDouble());
+        System.out.println("Area of circle: " + area);
+
+        System.out.println("Parallelogram: ");
+        System.out.print("Enter base and height of parallelogram: ");
+        area = area(sc.nextDouble(), sc.nextDouble());
+        System.out.println("Area of parallelogram: " + area);
+    }
+    static double area(double a, double b, double c) {
+        double s = (a+b+c) / 2;
+        return Math.sqrt(s*(s-a)*(s-b)*(s-c));
+    }
+    static double area(double radius) {
+        return Math.PI * radius * radius;
+    }
+    static double area(double base, double height) {
+        return base * height;
+    }
+
 }
--- Assignment_14.java
+++ Assignment_14.java
@@ -1,2 +1,41 @@
+//Write a program in Java to sort the elements of list so that they are in ascending order.
+
+import java.util.Scanner;
+
 public class Assignment_14 {
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    static void sort(int[] arr){
+        int temp;
+        boolean swapped;
+        for(int i=0; i<arr.length; i++) { swapped = false;
+            for(int j=1; j<arr.length-i; j++) if(arr[j-1] > arr[j]) {
+                swap(arr, j-1, j); swapped = true;
+            }
+            if(!swapped)
+                break;
+        }
+        System.out.print("The sorted array is : ");
+        for (int j : arr) {
+            System.out.print(j +" ");
+        }
+    }
+    static void input(){
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        n=sc.nextInt();
+        int[] arr =new int[n];
+        System.out.println("Enter the terms ");
+        for (int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        sort(arr);
+    }
+    public static void main(String[] args) {
+        input();
+    }
 }
