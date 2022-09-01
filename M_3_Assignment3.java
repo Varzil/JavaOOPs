7e20993e08293d1f4bcb9391e29e4c992db7cee7
Varzil <varzilthakkarvvv@gmail.com>
2022-09-01 11:47:34 +0530

    Adding Module 2 Assignment 3 💻

---
+++ M_3_Assignment3.java
@@ -0,0 +1,10 @@
+// Write a program in Java in which a subclass constructor invokes the constructor of the super class
+// and instantiate the values.
+
+public class M_3_Assignment3 {
+    int n;
+    M_3_Assignment3(int n){
+        this.n=n;
+        System.out.println(n);
+    }
+}
