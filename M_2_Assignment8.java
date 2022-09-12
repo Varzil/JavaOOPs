3c4e4e9e0706ed132912d5d749fe21535d8461d4
Varzil <varzilthakkarvvv@gmail.com>
2022-09-12 08:34:17 +0530

    Adding Module 2 Assignment 8 💻

---
+++ M_2_Assignment8.java
@@ -0,0 +1,22 @@
+// Write   a   program that   implements two   constructors   in   the   class.
+// We   call   the   other constructor using ‘this’ pointer, from the default constructor of the class.
+
+public class M_2_Assignment8 {
+    public int x,y;
+    public M_2_Assignment8(){
+        this(10,20);
+        x=5;
+        y=7;
+    }
+
+    public M_2_Assignment8(int a,int b){
+        System.out.println("Parameterised constructor called using this");
+        x=a;
+        y=b;
+    }
+
+    public static void main(String[] args) {
+        M_2_Assignment8 obj=new M_2_Assignment8();
+
+    }
+}
