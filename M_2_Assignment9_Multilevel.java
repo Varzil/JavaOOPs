bd7acc30bc6a9d3a2d7051cc1e65d11685f3de2f
Varzil <varzilthakkarvvv@gmail.com>
2022-09-12 08:50:20 +0530

    Adding Module 2 Assignment 9 MultiLevel Inheritance 💻

---
+++ M_2_Assignment9_Multilevel.java
@@ -0,0 +1,24 @@
+public class M_2_Assignment9_Multilevel {
+    public static void main(String[] args) {
+        Puppy pup=new Puppy();
+        pup.age();
+        pup.eat();
+        pup.Trait();
+    }
+}
+class Animals{
+    void eat(){
+        System.out.println("Eating....");
+    }
+}
+class Dogs extends Animals{
+    void Trait(){
+        System.out.println("Ruthless.....");
+    }
+
+}
+class Puppy extends Dogs{
+    void age(){
+        System.out.println("5 Months Age ✌");
+    }
+}
