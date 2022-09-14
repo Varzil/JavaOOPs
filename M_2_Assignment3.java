57fc45b66ed59c14c4b0fe09fc5479daa9fe2ca4
Varzil <varzilthakkarvvv@gmail.com>
2022-09-14 14:25:30 +0530

    Changing Names 💻

--- M_2_Assignment3.java
+++ M_2_Assignment3.java
@@ -1,9 +1,9 @@
 // Write a program in Java in which a subclass constructor invokes the constructor of the super class
 // and instantiate the values.
 
-public class M_3_Assignment3 {
+public class M_2_Assignment3 {
     int n;
-    M_3_Assignment3(int n){
+    M_2_Assignment3(int n){
         this.n=n;
         System.out.println(n);
     }
--- M_2_Assignment3_Child.java
+++ M_2_Assignment3_Child.java
@@ -1,15 +1,15 @@
 // This is the child class calling the parent class and invoking the constructor
 import java.util.Scanner;
 
-public class M_3_Assignment3_Child extends M_3_Assignment3 {
+public class M_2_Assignment3_Child extends M_2_Assignment3 {
 
-    M_3_Assignment3_Child(int n) {
+    M_2_Assignment3_Child(int n) {
         super(n);
     }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        M_3_Assignment3 obj=new M_3_Assignment3_Child(n);
+        M_2_Assignment3 obj=new M_2_Assignment3_Child(n);
     }
 }
