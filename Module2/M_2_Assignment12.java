e246ce1fc21818ca0bbbfe5316a65e087a33f76d
Varzil <varzilthakkarvvv@gmail.com>
2022-09-15 17:45:08 +0530

    Adding Assignment 12 Module 2 💻

---
+++ Module2/M_2_Assignment12.java
@@ -0,0 +1,16 @@
+//Write  a  program  to  compute  if  one  string  is  a  rotation  of  another.
+// For  example,  pit  is rotation of tip as pit has same character as tip.
+package Module2;
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class M_2_Assignment12 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str1=sc.nextLine();
+        String str2=sc.nextLine();
+
+
+    }
+}
