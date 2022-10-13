cf3273c883bfcb15b07efe11a864a7c22350e097
Varzil <varzilthakkarvvv@gmail.com>
2022-10-13 17:34:27 +0530

    Changing Assignment1 💻

--- Module3/Assignment1.java
+++ Module3/Assignment1.java
@@ -1,16 +1,6 @@
 package Module3;
 // Read a content from file: calculate number of sentences, words and characters from the file.
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Assignment1 {
-    public static void main(String[] args) throws IOException {
-        File file = new File("../demo.txt");
-        Scanner sc = new Scanner(file);
 
-        while (sc.hasNextLine())
-            System.out.println(sc.nextLine());
-    }
+
 }
