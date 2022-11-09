16657dae59feae16747867c2d8d750d719afe41d
Varzil <varzilthakkarvvv@gmail.com>
2022-11-09 10:10:12 +0530

    Changing AWT file💻

--- AWT/FirstFrame.java
+++ AWT/FirstFrame.java
@@ -3,6 +3,17 @@ import java.awt.*; // import all classes from java.awt package
 class FirstFrame {
     public static void main(String[] args) {
         Frame f =new Frame("Mera frame"); // create a frame
+        Button b = new Button("Click me"); // create a button
+        TextField tf =new TextField(20); // create a text field
+        Label l=new Label("Enter your name"); // create a label
+
+        f.setSize(300,300); // set the size of frame
+        f.setLayout(new FlowLayout()); // set the layout of frame
+
+        f.add(l); // add label to frame
+        f.add(tf); // add text field to frame
+        f.add(b); // add button to frame
+
         f.setVisible(true); // make frame visible
     }
 }
