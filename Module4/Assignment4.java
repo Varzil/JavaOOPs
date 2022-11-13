435019776fe951165ad54b9a143bb175e750fe17
Varzil <varzilthakkarvvv@gmail.com>
2022-11-13 19:49:58 +0530

    Adding files 💻

--- Module4/Assignment4.java
+++
@@ -1,9 +0,0 @@
-package Module4;
-
-// Write a program to generate a window without an applet window using main() function.
-
-public class Assignment4 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ Module4/Assignment8.java
@@ -0,0 +1,28 @@
+package Module4;
+import java.awt.*;
+import java.awt.event.*;
+
+// WAP to create a Dialogbox.
+public class Assignment8 {
+    private static Dialog d;
+    Assignment8() {
+        Frame f= new Frame();
+        d = new Dialog(f , "Dialog Example", true);
+        d.setLayout( new FlowLayout() );
+        Button b = new Button ("OK");
+        b.addActionListener ( new ActionListener()
+        {
+            public void actionPerformed( ActionEvent e )
+            {
+                Assignment8.d.setVisible(false);
+            }
+        });
+        d.add( new Label ("Click button to continue."));
+        d.add(b);
+        d.setSize(300,300);
+        d.setVisible(true);
+    }
+    public static void main(String args[]){
+        new Assignment8();
+    }
+}
